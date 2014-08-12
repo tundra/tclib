d63993b26b9ea8c507550cfae11f9a4c51b73650
Christian Plesner Hansen <c7n@p5r.org>
2014-08-12 15:16:23 +0200

    Self-review

--- src/c/sync/thread.h
+++ src/c/sync/thread.h
@@ -6,14 +6,21 @@
 
 #include "stdc.h"
 
+// Opaque thread type.
 struct native_thread_t;
 
+// Creates and returns a new native thread that will run the given callback
+// with the given data when started.
 native_thread_t *new_native_thread(void *(callback)(void*), void *data);
 
+// Disposes the given native thread.
 void dispose_native_thread(native_thread_t *thread);
 
+// Starts the given thread running.
 bool native_thread_start(native_thread_t *thread);
 
+// Waits for the given thread to finish, then returns the result returned from
+// the thread's callback.
 void *native_thread_join(native_thread_t *thread);
 
 #endif // _TCLIB_THREAD_H
