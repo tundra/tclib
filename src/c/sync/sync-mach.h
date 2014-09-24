8540393a567651962e8bb052594e63fe42c4ea64
Christian Plesner Hansen <c7n@p5r.org>
2014-09-24 13:52:42 +0200

    Whoops, forgot a file.

---
+++ src/c/sync/sync-mach.h
@@ -0,0 +1,11 @@
+//- Copyright 2014 the Neutrino authors (see AUTHORS).
+//- Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+#include <mach/semaphore.h>
+#include <mach/mach.h>
+
+#include "sync-posix.h"
+
+// This is yucky but this isn't a public header so I'll allow it.
+#define platform_semaphore_t mach_platform_semaphore_t
+typedef semaphore_t mach_platform_semaphore_t;
