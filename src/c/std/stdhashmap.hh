860ce00ba1c7272be8f21709cd7882e633716469
Christian Plesner Hansen <c7n@p5r.org>
2014-12-18 17:35:08 +0100

    Self-review

--- src/c/std/stdhashmap.hh
+++ src/c/std/stdhashmap.hh
@@ -9,6 +9,9 @@
 #  pragma warning(pop)
 #  define platform_hash_map std::hash_map
 #else
+// I'm pretty sure this is not how you want to do this but a cursory google
+// search didn't give a good answer to what you're really supposed to do it and
+// this works. Yucky though.
 #  define _GLIBCXX_PERMIT_BACKWARD_HASH
 #  include <hash_map>
 #  define platform_hash_map __gnu_cxx::hash_map
