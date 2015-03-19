17aed580c2f68feead6a0105dd3fa9774bc89380
Christian Plesner Hansen <c7n@p5r.org>
2015-03-19 11:33:38 +0100

    Touch

--- tests/c/test_string.cc
+++ tests/c/test_string.cc
@@ -131,7 +131,7 @@ static void format_test_value(format_handler_o *self, format_request_t *request,
 }
 
 TEST(string, custom_formatter) {
-  return;
+  return; // foo
   format_handler_o_vtable_t vtable = { format_test_value };
   test_format_handler_o handler;
   handler.super.header.vtable = &vtable;
