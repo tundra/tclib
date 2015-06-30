3aa72b4a1364a6e03f52c70a510b0d534bdd8a45
Christian Plesner Hansen <plesner@t.undra.org>
2015-06-30 22:03:46 +0200

    Maybe fix problem on older GCCs.

--- src/c/c/stdc-posix.h
+++ src/c/c/stdc-posix.h
@@ -19,6 +19,14 @@
 #  error "Can't determine the pointer size."
 #endif
 
+#define GCC_VERSION (__GNUC__ * 10000  + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+
+#if GCC_VERSION >= 40700
+#  define IF_GCC_4_7(T, E) T
+#else
+#  define IF_GCC_4_7(T, E) E
+#endif
+
 // You might reasonably want this to be ALWAYS_INLINE but that just gets soooo
 // shouty and it looks bad mixed with other lower-case modifiers.
 #ifdef IS_GCC
--- src/c/sync/atomic-posix.c
+++ src/c/sync/atomic-posix.c
@@ -1,10 +1,13 @@
 //- Copyright 2015 the Neutrino authors (see AUTHORS).
 //- Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+// The name of the add-and-fetch builtin changed in GCC 4.7.
+#define add_and_fetch_builtin IF_GCC_4_7(__atomic_add_fetch, __sync_add_and_fetch)
+
 int32_t atomic_int32_increment(atomic_int32_t *value) {
-  return __atomic_add_fetch(&value->value, 1, __ATOMIC_RELAXED);
+  return add_and_fetch_builtin(&value->value, 1, __ATOMIC_RELAXED);
 }
 
 int32_t atomic_int32_decrement(atomic_int32_t *value) {
-  return __atomic_add_fetch(&value->value, -1, __ATOMIC_RELAXED);
+  return add_and_fetch_builtin(&value->value, -1, __ATOMIC_RELAXED);
 }
