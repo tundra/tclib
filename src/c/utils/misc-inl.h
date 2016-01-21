6a9e45574f01b399f32ea6ee1d4213cb5302e248
Christian Plesner Hansen <plesner@t.undra.org>
2016-01-21 11:58:29 +0100

    Added 64-bit align.

--- src/c/utils/misc-inl.h
+++ src/c/utils/misc-inl.h
@@ -6,8 +6,16 @@
 
 #include "c/stdc.h"
 
-always_inline bool is_power_of_2(uint64_t value) {
+// Returns true iff the given value is a power of 2. Arbitrarily returns true on
+// 0.
+static always_inline bool is_power_of_2(uint64_t value) {
   return (value == 1ULL) || !(value & (value - 1));
+}
+
+// Returns the given value rounded up to the nearest multiple of the given
+// alignment which must be a power of 2.
+static always_inline uint64_t align_uint64(uint64_t alignment, uint64_t value) {
+  return (value + (alignment - 1ULL)) & (~(alignment - 1ULL));
 }
 
 #endif // _UTILS_MISC_H
