0e9f95f8280f5b7a0fbfd5b1dc7b1643f3191244
Christian Plesner Hansen <plesner@t.undra.org>
2016-02-26 10:03:15 +0100

    Move some utility stuff from neutrino to tclib.

--- src/c/utils/misc-inl.h
+++ src/c/utils/misc-inl.h
@@ -18,4 +18,36 @@ static always_inline uint64_t align_uint64(uint64_t alignment, uint64_t value) {
   return (value + (alignment - 1ULL)) & (~(alignment - 1ULL));
 }
 
+// Expands to an expression that yields the given size rounded up to the nearest
+// multiple of the given alignment. This evaluates the alignment twice and may
+// mess with the types involved (64/32 bits) so if at all possible use
+// align_size instead.
+#define STATIC_ALIGN_SIZE(ALGN, SIZE) (((SIZE) + ((ALGN) - 1)) & (~((ALGN) - 1)))
+
+// Returns a pointer greater than or equal to the given pointer which is
+// aligned to an 'alignment' boundary.
+static size_t align_size(size_t alignment, size_t size) {
+  return STATIC_ALIGN_SIZE(alignment, size);
+}
+
+// Returns the greatest of a and b.
+static always_inline size_t max_size(size_t a, size_t b) {
+  return (a < b) ? b : a;
+}
+
+// Returns the smallest of a and b.
+static always_inline size_t min_size(size_t a, size_t b) {
+  return (a < b) ? a : b;
+}
+
+// Returns the greatest of a and b.
+static always_inline int64_t max_64(int64_t a, int64_t b) {
+  return (a < b) ? b : a;
+}
+
+// Returns the smallest of a and b.
+static always_inline int64_t min_64(int64_t a, int64_t b) {
+  return (a < b) ? a : b;
+}
+
 #endif // _UTILS_MISC_H
